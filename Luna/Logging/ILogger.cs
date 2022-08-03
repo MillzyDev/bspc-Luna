d23680f1bb0a17ea3cb1031308082bd22e9bc601
Millzy <millzyyt08@gmail.com>
2022-08-03 11:13:50 +0100

    logging

---
+++ Luna/Logging/ILogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Luna.Logging
+{
+    internal interface ILogger : IInitializable
+    {
+        void Critical(string message);
+        void Critical(Exception e);
+
+        void Error(string message);
+        void Error(Exception e);
+
+        void Warn(string message);
+        void Warn(Exception e);
+
+        void Info(string message);
+        void Info(Exception e);
+
+        void Debug(string message);
+        void Debug(Exception e);
+    }
+}
---
+++ Luna/Logging/Logger.cs
@@ -0,0 +1,67 @@
+﻿using Luna.Configuration;
+using System;
+
+namespace Luna.Logging
+{
+    internal class Logger : ILogger
+    {
+        private static Logger _instance;
+
+        private Logger(ref PluginConfig)
+
+        public static Logger GetInstance(ref PluginConfig config = null)
+        {
+            if (_instance == null)
+        }
+
+        public void Critical(string message)
+        {
+            if ()
+        }
+
+        public void Critical(Exception e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Debug(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Debug(Exception e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(Exception e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(Exception e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(Exception e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
