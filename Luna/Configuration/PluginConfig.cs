84af836ef124f448b7001072d3cacf6722b489f7
Millzy <millzyyt08@gmail.com>
2022-08-03 13:40:18 +0100

    better config

--- Luna/Configuration/PluginConfig.cs
+++ Luna/Configuration/PluginConfig.cs
@@ -2,23 +2,22 @@
 using System.Runtime.CompilerServices;
 using IPA.Config.Stores;
 
+using IPALogLevel = IPA.Logging.Logger.Level;
+
 [assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
 namespace Luna.Configuration
 {
     internal class PluginConfig
     {
-        public virtual Logging Logger { get; set; }
 
         public virtual bool Enabled { get; set; } = true;
-
-        public class Logging
-        {
-            public virtual bool LogInfo { get; set; } = true;
-            public virtual bool LogWarn { get; set; } = true;
-            public virtual bool LogError { get; set; } = true;
-            public virtual bool LogCritical { get; set; } = true;
-            public virtual bool LogDebug { get; set; } = false;
-
-        }
+        public virtual IPALogLevel[] AllowedLogLevels { get; set; } = 
+        { 
+            IPALogLevel.Info, 
+            IPALogLevel.Warning, 
+            IPALogLevel.Error, 
+            IPALogLevel.Critical 
+        };
+       
     }
 }
