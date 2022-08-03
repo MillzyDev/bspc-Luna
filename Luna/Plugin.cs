055e1b75907eddef7d2dc93ff7475e38f239c87d
Millzy <millzyyt08@gmail.com>
2022-08-03 16:40:24 +0100

    deleteus

--- Luna/Plugin.cs
+++ Luna/Plugin.cs
@@ -1,11 +1,7 @@
 ﻿using IPA;
-using IPA.Config;
-using IPA.Config.Stores;
 using Luna.Configuration;
 using Luna.Installers;
-using Newtonsoft.Json;
 using SiraUtil.Zenject;
-using System.IO;
 using IPALogger = IPA.Logging.Logger;
 
 namespace Luna
@@ -24,38 +20,13 @@ namespace Luna
             Instance = this;
             VanillaLogger = logger;
 
-            LoadConfig(out PluginConfig config);
-            Config = config;
+            Config = PluginConfig.Load();
 
             zenjector.UseLogger(logger);
 
-            #region Install Installers
-
+            #region Installers
             zenjector.Install<AppInstaller>(Location.App);
-
             #endregion
-        }
-
-        void LoadConfig(out PluginConfig pluginConfig)
-        {
-            if (!File.Exists(PluginConfig.ConfigPath))
-            {
-                pluginConfig = new PluginConfig();
-
-                using (var file = File.CreateText(PluginConfig.ConfigPath))
-                {
-                    string text = JsonConvert.SerializeObject(pluginConfig, Formatting.Indented);
-                    VanillaLogger.Info(text);
-
-                    file.Write(text);
-                    return;
-                }
-            }
-
-            string json = File.ReadAllText(PluginConfig.ConfigPath);
-
-
-            pluginConfig = JsonConvert.DeserializeObject<PluginConfig>(json);
         }
     }
 }
