d5d2555496770a902f473ebda52df725c7763577
Millzy <millzyyt08@gmail.com>
2022-08-03 13:40:56 +0100

    OnApp

---
+++ Luna/Installers/AppInstaller.cs
@@ -0,0 +1,15 @@
+﻿using Luna.Configuration;
+using Luna.Logging;
+using Zenject;
+using IPALogger = IPA.Logging.Logger;
+
+namespace Luna.Installers
+{
+    internal class AppInstaller : Installer
+    {
+        public override void InstallBindings()
+        {
+            Logger.BindToContainer(Container);
+        }
+    }
+}
---
+++ Luna/Managers/OnAppManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace Luna.Managers
+{
+    internal class OnAppManager : IInitializable
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
