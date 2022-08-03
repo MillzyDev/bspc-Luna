8bdccddb55d994bd4acf272b56c08a13ac5d8236
Millzy <millzyyt08@gmail.com>
2022-08-03 11:13:59 +0100

    log config

---
+++ Luna/Configuration/PluginConfig.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Runtime.CompilerServices;
+using IPA.Config.Stores;
+
+[assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
+namespace Luna.Configuration
+{
+    internal class PluginConfig
+    {
+        public virtual Logging Logger { get; set; }
+
+        public virtual bool Enabled { get; set; } = true;
+
+        public class Logging
+        {
+            public virtual bool LogInfo { get; set; } = true;
+            public virtual bool LogWarn { get; set; } = true;
+            public virtual bool LogError { get; set; } = true;
+            public virtual bool LogCritical { get; set; } = true;
+            public virtual bool LogDebug { get; set; } = false;
+
+        }
+    }
+}
