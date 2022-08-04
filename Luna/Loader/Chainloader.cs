cb82fee95d1c4562158ab84ca3f9a3df88f3e424
Millzy <millzyyt08@gmail.com>
2022-08-04 09:53:19 +0100

    improve docs

--- Luna/Loader/Chainloader.cs
+++ Luna/Loader/Chainloader.cs
@@ -55,21 +55,32 @@ namespace Luna
         public AddonCollection OnInactiveMultiPlayer { get => onInactiveMultiPlayer; }
         #endregion
 
+        /// <summary>
+        /// Loads a singular lua or luac addon from a path.
+        /// </summary>
+        /// <param name="path">Path of the file, including the file name.</param>
         public void LoadAddon(string path)
         {
-
+            Lua state = new Lua();
+            state.LoadFile(path);
         }
 
         /// <summary>
-        /// Loads all lua 
+        /// Loads all lua or luac addons in a directory.
         /// </summary>
-        /// <param name="path"></param>
+        /// <param name="path">Path to the directory that contains the addon files.</param>
         public void LoadAddons(string path)
         {
 
         }
 
-        public void AddModule(string name, Action<Lua> onLoad, Action onUnload)
+        /// <summary>
+        /// Registers an action to be run when the lua <c>loadModule</c> function is used.
+        /// </summary>
+        /// <param name="name">The name of the module that will be used in the lua <c>loadModule</c> function and will be used to access your module</param>
+        /// <param name="onLoad">An action that will run when <c>loadModule(<paramref name="name"/>)</c> is invoked by an addon.</param>
+        /// <param name="onUnload">An action that will run once the execution of an entrypoint has concluded.</param>
+        public void RegisterModule(string name, Action<Lua> onLoad, Action onUnload = null)
         {
 
         }
