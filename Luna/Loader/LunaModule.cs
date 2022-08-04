1a326acacfdce311fdd85dc0b63697e4fe17289a
Millzy <millzyyt08@gmail.com>
2022-08-04 22:33:41 +0100

    WIP

---
+++ Luna/Loader/LunaModule.cs
@@ -0,0 +1,18 @@
+﻿using NLua;
+using System.Reflection;
+
+namespace Luna.Loader
+{
+    public abstract class LunaModule
+    {
+        protected Lua lua;
+
+        protected internal LunaModule(string name, Lua lua)
+        {
+            this.lua = lua;
+        }
+
+        public virtual void OnLoad(Lua lua) {}
+        public virtual void OnUnload(Lua lua) {}
+    }
+}
