18832663ab3a3fc15d42efdc1191830522b606e6
Millzy <millzyyt08@gmail.com>
2022-08-04 13:14:44 +0100

    LoaderException

---
+++ Luna/Loader/LoaderException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Luna.Loader
+{
+    public class LoaderException : Exception
+    {
+        public LoaderException() : base()
+            => AddToErrorsList();
+
+        public LoaderException(string message) : base(message)
+            => AddToErrorsList();
+
+        public LoaderException(string message, Exception innerException) : base(message, innerException)
+            => AddToErrorsList();
+
+        public LoaderException(SerializationInfo info, StreamingContext context) : base(info, context)
+            => AddToErrorsList();
+
+        private void AddToErrorsList()
+        {
+            Chainloader.Instance.LoaderErrors.Add(this);
+        }
+    }
+}
