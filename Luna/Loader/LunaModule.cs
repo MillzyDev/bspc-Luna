6891a52995930bebf826229eda032f15a88ad539
Millzy <millzyyt08@gmail.com>
2022-08-14 10:38:18 +0100

    removed temporarily

--- Luna/Loader/LunaModule.cs
+++ Luna/Loader/LunaModule.cs
@@ -1,18 +1,23 @@
-﻿using NLua;
-using System.Reflection;
+﻿//using NLua;
+//using System;
 
-namespace Luna.Loader
-{
-    public abstract class LunaModule
-    {
-        protected Lua lua;
+//namespace Luna.Loader
+//{
+//    public abstract class LunaModule
+//    {
+//        protected Lua lua;
 
-        protected internal LunaModule(string name, Lua lua)
-        {
-            this.lua = lua;
-        }
+//        protected internal LunaModule(string name, Lua lua)
+//        {
+//            this.lua = lua;
+//        }
 
-        public virtual void OnLoad(Lua lua) {}
-        public virtual void OnUnload(Lua lua) {}
-    }
-}
+//        protected void AddFunction(string name, Action func)
+//        {
+//            lua[name] = func;
+//        }
+
+//        public virtual void OnLoad() {}
+//        public virtual void OnUnload() {}
+//    }
+//}
